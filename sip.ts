dd2ffb2d9e948d61e03d3e69f84eb319425c4f1e
Derek Liu <yjkhtddx@sina.com>
2019-12-06 22:46:21 +0800

    sip

--- sip.ts
+++ sip.ts
@@ -48,9 +48,32 @@ export interface SIP_Parse_UIR{
     user?:string;
 }
 
+export interface SIP_Transport_Protocols{
+    UDP?:any;//TODO
+    TCP?:any;//TODO
+    TLS?:any;//TODO
+    WS?:any;//TODO
+}
 
+export class SIP_Transport
+{
+    private sendLog:SIP_Logger_Send = (message:SIP_Message,target:any)=>{};
+    private recvLog:SIP_Logger_Recv = (message:SIP_Message,remote:SIP_Remote)=>{};
+    private protocols:SIP_Transport_Protocols = {};
+    constructor(options:SIP_Server_Options,callback:SIP_Callback)
+    {
+        let callbackAndLog:SIP_Callback = callback;
+        if(options.logger && options.logger.recv){
+            callbackAndLog = (message:SIP_Message,remote:SIP_Remote,stream:Buffer)=>{
+                this.recvLog(message,remote);
+                callback(message,remote,stream);
+            }
+        }
+        
+    }
+}
 
-export class SIP_SERVER
+export class SIP_Server
 {
     private errorLog:SIP_Logger_Error = (error:Error)=>{};
     private sendLog:SIP_Logger_Send = (message:SIP_Message,target:any)=>{};
@@ -91,9 +114,6 @@ export class SIP_SERVER
         return 200;
     };
     public makeTransport(options:SIP_Server_Options,callback:SIP_Callback):any{
-        let callbackAndLog:SIP_Callback = (message:SIP_Message,remote:SIP_Remote,stream:Buffer)=>{
-            this.recvLog(message,remote);
-            callback(message,remote,stream);
-        }
+        
     }
 }

0d60d6a220779688508fb2920184976f271b8f7e
刘跃龙 <yjkhtddx@sina.com>
2019-12-09 18:01:34 +0800

    sip

--- sip.ts
+++ sip.ts
@@ -6,20 +6,25 @@ export interface SIP_Message_Headers {
     [key: string]: any;
 }
 
+export interface SIP_Message_Headers_Value{
+    s:string;
+    i:number;
+}
+
 export interface SIP_Message {
     method?: string;
     uri?: string;
     version?: string;
     status?: number;
     reason?: string;
-    headers?: SIP_Message_Headers;
+    headers: SIP_Message_Headers;
     content?: string;
     [key: string]: any;
 }
 
 
 function parseResponse(rs: string, m: SIP_Message): SIP_Message | undefined {
-    var r = rs.match(/^SIP\/(\d+\.\d+)\s+(\d+)\s*(.*)\s*$/);
+    var r:RegExpMatchArray | null = rs.match(/^SIP\/(\d+\.\d+)\s+(\d+)\s*(.*)\s*$/);
     if (r) {
         m.version = r[1];
         m.status = +r[2];
@@ -29,7 +34,7 @@ function parseResponse(rs: string, m: SIP_Message): SIP_Message | undefined {
 }
 
 function parseRequest(rq: string, m: SIP_Message): SIP_Message | undefined {
-    var r = rq.match(/^([\w\-.!%*_+`'~]+)\s([^\s]+)\sSIP\s*\/\s*(\d+\.\d+)/);
+    var r:RegExpMatchArray | null = rq.match(/^([\w\-.!%*_+`'~]+)\s([^\s]+)\sSIP\s*\/\s*(\d+\.\d+)/);
     if (r) {
         m.method = unescape(r[1]);
         m.uri = r[2];
@@ -38,9 +43,9 @@ function parseRequest(rq: string, m: SIP_Message): SIP_Message | undefined {
     }
 }
 
-function applyRegex(regex:RegExp, data) {
+function applyRegex(regex:RegExp, data:SIP_Message_Headers_Value):RegExpExecArray|undefined {
     regex.lastIndex = data.i;
-    var r = regex.exec(data.s);
+    var r:RegExpExecArray | null = regex.exec(data.s);
 
     if (r && (r.index === data.i)) {
         data.i = regex.lastIndex;
@@ -48,7 +53,7 @@ function applyRegex(regex:RegExp, data) {
     }
 }
 
-function parseParams(data:string, hdr) {
+function parseParams(data:SIP_Message_Headers_Value, hdr:any) {
     hdr.params = hdr.params || {};
 
     var re = /\s*;\s*([\w\-.!%*_+`'~]+)(?:\s*=\s*([\w\-.!%*_+`'~]+|"[^"\\]*(\\.[^"\\]*)*"))?/g;
@@ -60,7 +65,7 @@ function parseParams(data:string, hdr) {
     return hdr;
 }
 
-function parseMultiHeader(parser, d, h) {
+function parseMultiHeader(parser, d:SIP_Message_Headers_Value, h:string[]|undefined) {
     h = h || [];
 
     var re = /\s*,\s*/g;
@@ -71,35 +76,52 @@ function parseMultiHeader(parser, d, h) {
     return h;
 }
 
-function parseGenericHeader(d, h) {
+/**
+ * 
+ * @param d 数据和一个number？，number应该时数组时使用
+ * @param h 已解析的制定头域，如果已有值，新的值用逗号分割添加到原来的值后面
+ */
+function parseGenericHeader(d:SIP_Message_Headers_Value, h:string):string {
     return h ? h + ',' + d.s : d.s;
 }
 
 export interface SIP_AOR{
     name?:string;
-    uri?:string;
+    uri?:string|;
     [key:string]:any;
 };
 
-export function parseAOR(data:string):SIP_AOR {
-    var r = applyRegex(/((?:[\w\-.!%*_+`'~]+)(?:\s+[\w\-.!%*_+`'~]+)*|"[^"\\]*(?:\\.[^"\\]*)*")?\s*\<\s*([^>]*)\s*\>|((?:[^\s@"<]@)?[^\s;]+)/g, data);
-    return parseParams(data, { name: r[1], uri: r[2] || r[3] || '' });
+export function parseAOR(data:SIP_Message_Headers_Value):SIP_AOR|undefined {
+    var r:RegExpExecArray|undefined = applyRegex(/((?:[\w\-.!%*_+`'~]+)(?:\s+[\w\-.!%*_+`'~]+)*|"[^"\\]*(?:\\.[^"\\]*)*")?\s*\<\s*([^>]*)\s*\>|((?:[^\s@"<]@)?[^\s;]+)/g, data);
+    if(r){
+        return parseParams(data, { name: r[1], uri: r[2] || r[3] || '' });
+    }
 };
 
-function parseAorWithUri(data) {
-    var r = parseAOR(data);
-    r.uri = parseUri(r.uri);
+function parseAorWithUri(data:SIP_Message_Headers_Value) {
+    var r:SIP_AOR|undefined = parseAOR(data);
+    if(r){
+        r.uri = parseUri(r.uri);
+    }
     return r;
 }
 
-function parseVia(data) {
-    var r = applyRegex(/SIP\s*\/\s*(\d+\.\d+)\s*\/\s*([\S]+)\s+([^\s;:]+)(?:\s*:\s*(\d+))?/g, data);
+function parseVia(data:SIP_Message_Headers_Value) {
+    var r: = applyRegex(/SIP\s*\/\s*(\d+\.\d+)\s*\/\s*([\S]+)\s+([^\s;:]+)(?:\s*:\s*(\d+))?/g, data);
     return parseParams(data, { version: r[1], protocol: r[2], host: r[3], port: r[4] && +r[4] });
 }
 
-function parseCSeq(d) {
-    var r = /(\d+)\s*([\S]+)/.exec(d.s);
-    return { seq: +r[1], method: unescape(r[2]) };
+export interface SIP_CSeq{
+    seq:number;
+    method:string;
+}
+
+
+function parseCSeq(d:SIP_Message_Headers_Value):SIP_CSeq|undefined {
+    var r:RegExpExecArray | null = /(\d+)\s*([\S]+)/.exec(d.s);
+    if(r){
+        return { seq: +r[1], method: unescape(r[2]) };
+    }
 }
 
 function parseAuthHeader(d) {
@@ -130,7 +152,11 @@ function parseAuthenticationInfoHeader(d) {
 /**
 * 换头域名称,似乎是兼容性逻辑
 */
-const compactForm: any = {
+interface SIP_CompactFormType{
+    [key:string]:string
+}
+
+const compactForm: SIP_CompactFormType = {
     i: 'call-id',
     m: 'contact',
     e: 'contact-encoding',
@@ -143,20 +169,28 @@ const compactForm: any = {
     v: 'via'
 };
 
-let parsers:any = {
+interface SIP_Parsers_Function{
+    (d:SIP_Message_Headers_Value,h:string):number|string|SIP_AOR;
+}
+
+interface SIP_Parsers_Function_List{
+    [key:string]:SIP_Parsers_Function
+}
+
+let parsers:SIP_Parsers_Function_List = {
     'to': parseAOR,
     'from': parseAOR,
-    // 'contact': function (v, h) {
-    //     if (v == '*')
-    //         return v;
-    //     else
-    //         return parseMultiHeader(parseAOR, v, h);
-    // },
+    'contact': function (v:SIP_Message_Headers_Value, h:string) {
+        if (v.s == '*')
+            return v.s;
+        else
+            return parseMultiHeader(parseAOR, v, h);
+    },
     'route': parseMultiHeader.bind(0, parseAorWithUri),
     'record-route': parseMultiHeader.bind(0, parseAorWithUri),
     'path': parseMultiHeader.bind(0, parseAorWithUri),
     'cseq': parseCSeq,
-    // 'content-length': function (v) { return +v.s; },
+    'content-length': function (v:SIP_Message_Headers_Value) { return +v.s; },
     'via': parseMultiHeader.bind(0, parseVia),
     'www-authenticate': parseMultiHeader.bind(0, parseAuthHeader),
     'proxy-authenticate': parseMultiHeader.bind(0, parseAuthHeader),
@@ -213,7 +247,8 @@ export function stringify(message: SIP_Message): string {
 }
 
 function parse(data: string): SIP_Message | undefined {
-    let data_array = data.split(/\r\n(?![ \t])/);
+    let regHeadersLine:RegExp = /\r\n(?![ \t])/;
+    let data_array = data.split(regHeadersLine);//分行
 
     if (data_array[0] === '')
         return;
@@ -226,7 +261,7 @@ function parse(data: string): SIP_Message | undefined {
     m.headers = {};
 
     for (var i = 1; i < data_array.length; ++i) {
-        var r = data_array[i].match(/^([\S]*?)\s*:\s*([\s\S]*)$/);
+        var r:RegExpMatchArray | null = data_array[i].match(/^([\S]*?)\s*:\s*([\s\S]*)$/);
         if (!r) {
             return;
         }
@@ -243,22 +278,59 @@ function parse(data: string): SIP_Message | undefined {
     return m;
 }
 
-export function parseMessage(s: Buffer) {
-    var r = s.toString('binary').match(/^\s*([\S\s]*?)\r\n\r\n([\S\s]*)$/);
+export interface SIP_URI{
+    schema:string;
+    user:string;
+    passworld:string;
+    host:string;
+    port:number;
+    params:any;
+    headers:any;
+}
+
+export function parseUri(s:string):SIP_URI {
+    if(typeof s === 'object')
+      return s;
+  
+    var re = /^(sips?):(?:([^\s>:@]+)(?::([^\s@>]+))?@)?([\w\-\.]+)(?::(\d+))?((?:;[^\s=\?>;]+(?:=[^\s?\;]+)?)*)(?:\?(([^\s&=>]+=[^\s&=>]+)(&[^\s&=>]+=[^\s&=>]+)*))?$/;
+  
+    var r:RegExpExecArray | null = re.exec(s);
+  
+    if(r) {
+      return {
+        schema: r[1],
+        user: r[2],
+        password: r[3],
+        host: r[4],
+        port: +r[5],
+        params: (r[6].match(/([^;=]+)(=([^;=]+))?/g) || [])
+          .map(function(s) { return s.split('='); })
+          .reduce(function(params, x) { params[x[0]]=x[1] || null; return params;}, {}),
+        headers: ((r[7] || '').match(/[^&=]+=[^&=]+/g) || [])
+          .map(function(s){ return s.split('=') })
+          .reduce(function(params, x) { params[x[0]]=x[1]; return params; }, {})
+      }
+    }
+  }
+
+export function parseMessage(s: Buffer):SIP_Message | undefined {
+    var r:RegExpMatchArray | null = s.toString('binary').match(/^\s*([\S\s]*?)\r\n\r\n([\S\s]*)$/);
+    //r[1] 请求行 + 请求头部
+    //r[2] 请求数据
     if (r) {
-        var m = parse(r[1]);
+        var m:SIP_Message | undefined = parse(r[1]);
 
-        //   if(m) {
-        //     if(m.headers['content-length']) {
-        //       var c = Math.max(0, Math.min(m.headers['content-length'], r[2].length));
-        //       m.content = r[2].substring(0, c);
-        //     }
-        //     else {
-        //       m.content = r[2];
-        //     }
+        if(m) {
+            if(m.headers['content-length']) {
+                var c:number = Math.max(0, Math.min(m.headers['content-length'], r[2].length));
+                m.content = r[2].substring(0, c);
+            }
+            else {
+                m.content = r[2];
+            }
 
-        //     return m;
-        //   }
+            return m;
+        }
     }
 }
 
